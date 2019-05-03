68bbf10eab12f874865e249d01772b12dd633576
clem-HERITIER <clement.exe@gmail.com>
2019-05-03 12:15:23 +0200

    ajout des modifs precedentes a customer

--- Controller/CustomerStatisticController.php
+++ Controller/CustomerStatisticController.php
@@ -12,6 +12,7 @@
 
 namespace Statistic\Controller;
 
+use DateInterval;
 use Thelia\Controller\Admin\BaseAdminController;
 use Thelia\Core\Security\AccessManager;
 use Thelia\Model\CustomerQuery;
@@ -33,18 +34,80 @@ class CustomerStatisticController extends BaseAdminController
             return $response;
         }
 
+        // récupération des paramètres
+        $startMonth = $this->getRequest()->query->get('monthStart', date('m'));
+        $startYear = $this->getRequest()->query->get('yearStart', date('m'));
+        $endMonth = $this->getRequest()->query->get('monthEnd', date('m'));
+        $endYear = $this->getRequest()->query->get('yearEnd', date('m'));
+        $ghostCurve = $this->getRequest()->query->get('ghostCurve');
+
+        // Vérification des paramètres, renvoie un message d'erreur si le mois de fin est incorrect
+        if($startYear === $endYear && $endMonth < $startMonth)
+        {
+            $error = $this->getTranslator()->trans( "Error : End month is incorrect." );
+            return $this->jsonResponse(json_encode($error));
+        }
+
+        // Création date de début et date de fin
+        $startDate = new \DateTime($startYear . '-' . $startMonth . '-01');
+        /** @var \DateTime $endDate */
+        $endDate = clone($startDate);
+        $endDate->add(new DateInterval('P' . (cal_days_in_month(CAL_GREGORIAN, $endMonth, $endYear)-1) . 'D'));
+
         $data = new \stdClass();
 
-        $data->title = $this->getTranslator()->trans("Stats on %month/%year", array('%month' => $this->getRequest()->query->get('month', date('m')), '%year' => $this->getRequest()->query->get('year', date('Y'))));
-
+        // Change le titre en fonction de la période analysée
+        if ($startMonth === $endMonth)
+        {
+            $data->title = $this->getTranslator()->trans("Stats on %monthStart/%yearStart", array('%monthStart' => $this->getRequest()->query->get('monthStart', date('m')), '%yearStart' => $this->getRequest()->query->get('yearStart', date('Y'))));
+        }
+        else
+        {
+            $data->title = $this->getTranslator()->trans("Stats for beginning of %monthStart/%yearStart to end of %monthEnd/%yearEnd", array('%monthStart' => $this->getRequest()->query->get('monthStart', date('m')), '%yearStart' => $this->getRequest()->query->get('yearStart', date('Y')), '%monthEnd' => $this->getRequest()->query->get('monthEnd', date('m')), '%yearEnd' => $this->getRequest()->query->get('yearEnd', date('Y'))));
+        }
 
         /* new customers */
         $newCustomerSeries = new \stdClass();
         $newCustomerSeries->color = $this->getRequest()->query->get('customers_color', '#f39922');
+        /*
         $newCustomerSeries->graph = CustomerQuery::getMonthlyNewCustomersStats(
             $this->getRequest()->query->get('month', date('m')),
             $this->getRequest()->query->get('year', date('Y'))
         );
+        */
+
+        // Récupére les données pour chaques jours et les injecte dans un tableau
+        $dayCount = 0;
+        $stats = array();
+
+        if ($startYear !== $endYear)
+        {
+            for ($year=$startYear; $year<=$endYear; $year++)
+            {
+                if ($year < $endYear)
+                {
+                    for ($month=$startMonth; $month<=12; $month++)
+                    {
+                        $newCustomerSeries->graph = CustomerQuery::getMonthlyNewCustomersStats($month,$year);
+                    }
+                }
+                else
+                {
+                    for ($month=1; $month<=$endMonth; $month++)
+                    {
+                        $newCustomerSeries->graph = CustomerQuery::getMonthlyNewCustomersStats($month,$year);
+                    }
+                }
+            }
+        }
+        else
+        {
+            for ($month=$startMonth; $month<=$endMonth; $month++)
+            {
+                $newCustomerSeries->graph = CustomerQuery::getMonthlyNewCustomersStats($month,$endYear);
+            }
+        }
+
 
 //        /* first order */
 //        $firstOrderSeries = new \stdClass();
