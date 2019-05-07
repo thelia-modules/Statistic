c54585e91d4459c501874b769f8bea43be0d54a2
clem-HERITIER <clement.exe@gmail.com>
2019-05-07 16:18:42 +0200

    done

--- Statistic.php
+++ Statistic.php
@@ -8,6 +8,9 @@
 /*                                                                                   */
 /*      For the full copyright and license information, please view the LICENSE.txt  */
 /*      file that was distributed with this source code.                             */
+/*                                                                                   */
+/*       Contributors : - Romain Petit & Héritier Clément - Students                 */
+/*                                                                                   */
 /*************************************************************************************/
 
 namespace Statistic;
