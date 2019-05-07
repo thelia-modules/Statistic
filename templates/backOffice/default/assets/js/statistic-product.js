34f8c31881f490e3842d668ceaae42e9a07b7f96
Romain Petit <rpetit@openstudio.fr>
2019-05-07 11:49:23 +0200

    product stat

--- templates/backOffice/default/assets/js/statistic-product.js
+++ templates/backOffice/default/assets/js/statistic-product.js
@@ -3,21 +3,21 @@
  */
 
 (function ($) {
-    $(document).ready(function(){
+    $(document).ready(function () {
         var productDatedate = new Date();
         var productUrl = baseAdminUrl + '/module/statistic/product/turnover';
         var id = "jqplot-product";
         var jQPlotInstanceProduct; // global instance
         var productDate = new Date();
 
-        $('.product-date-picker').datepicker( {
+        $('.product-date-picker').datepicker({
             format: 'yyyy',
-            minViewMode: "years",
+            minViewMode: 1,
             language: "fr"
-        }).on('changeDate', function(e){
+        }).on('changeDate', function (e) {
             productDate = e.date;
             productyear = productDate.getFullYear();
-            var url =  $('.product-graph-select').data("url");
+            var url = $('.product-graph-select').data("url");
 
             setDataPlot(productUrl, id);
         });
@@ -26,6 +26,7 @@
 
 
         function setDataPlot(url, chartId) {
+
             var jQplotDate = productDate;
             jQplotDate.setDate(1); // Set day to 1 so we can add month without 30/31 days of month troubles.
             var jQplotData; // json data
@@ -35,23 +36,23 @@
             var jQPlotsOptions = {
                 animate: true,
                 axesDefaults: {
-                    tickOptions: {showMark: true, showGridline: true}
+                    tickOptions: { showMark: true, showGridline: true }
                 },
                 axes: {
                     xaxis: {
                         borderColor: '#ccc',
                         ticks: [],
-                        tickOptions: {showGridline: false}
+                        tickOptions: { showGridline: false, showLabel: true }
                     },
                     yaxis: {
                         min: 0,
-                        tickOptions: {showGridline: true, showMark: false, showLabel: true, shadow: false}
+                        tickOptions: { showGridline: true, showMark: false, showLabel: true, shadow: false }
                     }
                 },
                 seriesDefaults: {
                     lineWidth: 3,
                     shadow: false,
-                    markerOptions: {shadow: false, style: 'filledCircle', size: 12}
+                    markerOptions: { shadow: false, style: 'filledCircle', size: 12 }
                 },
                 grid: {
                     background: '#FFF',
@@ -77,7 +78,7 @@
 
             $('[data-toggle="' + chartId + '"]').click(function () {
 
-                $(this).toggleClass('active');
+                $('#jqplot-product').toggleClass('active');
                 jsonSuccessLoad();
 
             });
@@ -93,7 +94,7 @@
 
             function retrieveJQPlotJson(productRef, month, productyear, callback) {
 
-                $.getJSON(url, {ref: productRef, month: month, year: productyear})
+                $.getJSON(url, { ref: productRef, month: month, year: productyear })
                     .done(function (data) {
                         jQplotData = data;
                         jsonSuccessLoad();
@@ -112,14 +113,14 @@
                 seriesColors.push(json.series[0].color);
 
                 // Number of days to display ( = graph.length in one serie)
-                if( typeof json.series[0].graphLabel === 'undefined' ){
+                if (typeof json.series[0].graphLabel === 'undefined') {
                     var days = json.series[0].graph.length;
                     // Add days to xaxis
                     for (var i = 0; i < days; ++i) {
-                        ticks.push([i, i]);
+                        ticks.push([i - 1, i]);
                     }
 
-                }else {
+                } else {
                     var days = json.series[0].graphLabel.length;
                     var val = json.series[0].graphLabel;
                     // Add days to xaxis
@@ -154,7 +155,7 @@
                 jQPlotInstanceProduct = $.jqplot(chartId, series, jQPlotsOptions);
 
                 $(window).bind('resize', function (event, ui) {
-                    jQPlotInstanceProduct.replot({resetAxes: true});
+                    jQPlotInstanceProduct.replot({ resetAxes: true });
                 });
 
             }
@@ -165,10 +166,10 @@
             $('.product-graph-select').removeClass('active');
             $(this).toggleClass('active');
             productUrl = this.dataset.url;
-            if (type.indexOf('jqplot')!==-1) {
-                if( type.indexOf(',')!==-1){
+            if (type.indexOf('jqplot') !== -1) {
+                if (type.indexOf(',') !== -1) {
                     id = this.dataset.toggle.split(',')[1];
-                }else{
+                } else {
                     id = this.dataset.toggle;
                 }
                 $('.jqplot-content').show();
@@ -177,22 +178,23 @@
         });
 
         // Modification de la liste des produits par catégories
-        $('#category-select').change(function(e){
+        $('#category-select').change(function (e) {
             var url = $('#product-select').data('url');
             $.ajax({
-                url: url + '?category='+this.value
-            }).success(function(data){
+                url: url + '?category=' + this.value
+            }).success(function (data) {
+
                 var i = 0;
                 $select = $('#product-select');
                 $select.html('<option value="">Produit...</option>');
-                for(var k in data){
+                for (var k in data) {
                     var prod = data[k];
-                    $select.append('<option value='+prod["Ref"]+'>'+prod["i18n_TITLE"]+'</option>')
+                    $select.append('<option value=' + prod["Ref"] + '>' + prod["i18n_TITLE"] + '</option>')
                 }
             });
         });
 
-        $("#product-select").change(function(e){
+        $("#product-select").change(function (e) {
             setDataPlot(productUrl, id);
         });
 
